7d3c98ee54b3debe54fe24be9662cf7d98bb2500
Leigh Komperda <leighkomperda@weber.edu>
2024-09-04 10:13:03 -0600

    04SepNotes

---
+++ module_1/terminal_practice.py
@@ -0,0 +1,9 @@
+'''
+pwd = present working directory
+ls = list of dir
+cd = change directory -> all the back to the top
+tab to autocomplete with correct slashes and stuff
+. = pwd
+.. = parent
+
+'''
